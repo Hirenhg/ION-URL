024d2d2e077aebc94ecc56194b0eecec70164296
Hiren Gabu <hiren.gabu@ics-global.in>
2024-08-16 11:45:18 +0530

    Header and footer fixed in top bottom and url change in server

--- SERVER/popup.js
+++ SERVER/popup.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     chrome?.tabs?.query({ currentWindow: true, active: true }, function (tabs) {
         var tabURL = tabs[0].url; 
-        var iframeSrc = "http://10.227.254.169:5000?tabUrl=" + encodeURIComponent(tabURL);
+        var iframeSrc = "http://192.168.0.49:5000?tabUrl=" + encodeURIComponent(tabURL);
         $('#myIframe').attr('src', iframeSrc);
     });
 });
